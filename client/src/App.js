20ab9091dfa5aa7d6dcee177a9e99cab3cb0c44d
Jess Smith <ncicjhs9173@gmail.com>
2025-07-02 19:07:34 -0400

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
